b31d223f8890942172fc732cb9b7afee58d590d1
root <root@v22019076433293818.hotsrv.de>
2020-05-03 00:10:16 +0200

    added auto-disable on error

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Waiting for 5 minutes, until hopefully we have the correct time set.."
-sleep 300
+echo "Waiting for 10 seconds, until hopefully we have the correct time set.."
+sleep 10
 npm start
